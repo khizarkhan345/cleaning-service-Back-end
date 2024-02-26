fb71677971375097787303c5298b6cc1bc493424
Khizar Khan <khezarhayat881@gmail.com>
2024-02-26 13:17:35 -0500

    implemented the order change feature

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,9 +1,25 @@
 const order = require("../models/orderData");
-
+const customer = require("../models/customerData");
 exports.getData = (req, res, next) => {
-  res.json({
-    message: "Hello from cleaning info",
-  });
+  order
+    .findAll({
+      include: [
+        {
+          model: customer,
+          required: true,
+        },
+      ],
+    })
+    .then((result) => {
+      //console.log(result);
+      return res.json({
+        message: "Hello from cleaning info",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.postData = (req, res, next) => {
@@ -32,6 +48,52 @@ exports.postData = (req, res, next) => {
       return res.json({
         message: "Order data added successfully!",
       });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.changeOrder = (req, res, next) => {
+  const { id } = req.params;
+  const totalBedrooms = req.body.totalBedrooms;
+  const totalBathrooms = req.body.totalBathrooms;
+  const totalLivingrooms = req.body.totalLivingrooms;
+  const totalKitchens = req.body.totalKitchens;
+  const appointmentDate = req.body.appointmentDate;
+  const appointmentTime = req.body.appointmentTime;
+  const totalCost = req.body.totalCost;
+  const orderStatus = req.body.orderStatus;
+
+  order
+    .findByPk(id)
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json("Not Found");
+      }
+
+      let values = {
+        noOfBedrooms: parseInt(totalBedrooms),
+        noOfBathrooms: parseInt(totalBathrooms),
+        noOfLivingrooms: parseInt(totalLivingrooms),
+        noOfKitchens: parseInt(totalKitchens),
+        appointmentDate: appointmentDate,
+        appointmentTime: appointmentTime,
+        totalCost: totalCost,
+        orderStatus: orderStatus,
+      };
+
+      //result.dataValues.noOfBedrooms = parseInt(totalBedrooms);
+
+      return result.update(values).then((updatedResult) => {
+        console.log("updatedResult", updatedResult);
+      });
+      //return result.save();
+      // console.log(result.dataValues);
+    })
+    .then((data) => {
+      console.log("Changed");
+      res.status(200).json("Changed");
     })
     .catch((err) => {
       console.log(err);
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get("/", cleaningCont.getData);
 
 router.post("/addOrder", cleaningCont.postData);
+router.put("/changeOrder/:id", cleaningCont.changeOrder);
 
 module.exports = router;
